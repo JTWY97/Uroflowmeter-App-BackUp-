72067a6d0286c083c6fbffe38b5bc1f9fe878bdd
ThiaraSana <56861810+ThiaraSana@users.noreply.github.com>
2021-09-06 20:19:43 +0800

    test

--- Login.py
+++ Login.py
@@ -6,27 +6,17 @@
 # --> Patient: Log In Page, Main Page, Sypmtoms Tracker, Bladder Diary, Fluid Intake, Quick Pee
 # --> Doctor: Log In Page (Repeated), Doctor Sign Up Page, Main Page, Patient List, Patient Data Summary, Patient Sign Up Page
 
-# class MainApp(App):
-#     def build(self):
-#         label = Label(text='Hello from Kivy',
-#                       size_hint=(.5, .5),
-#                       pos_hint={'center_x': .5, 'center_y': .5})
-
-#         return label
-
-# if __name__ == '__main__':
-#     app = MainApp()
-#     app.run()
-
 from kivy.lang import Builder
 from kivymd.app import MDApp
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class MainApp(MDApp):
+
 	def build(self):
 		self.theme_cls.theme_style = "Dark"
 		self.theme_cls.primary_palette = "BlueGray"
-		return Builder.load_file('login.kv')
+		return Builder.load_file('Login.kv')
+
 	def logger(self):
 		self.root.ids.welcome_label.text = f'Sup {self.root.ids.user.text}!' #id: welcome_id
 
