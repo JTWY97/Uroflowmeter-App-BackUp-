3e388064dbb2dfcbc904807605daa2dd41746e33
ThiaraSana <56861810+ThiaraSana@users.noreply.github.com>
2021-09-10 10:09:11 +0800

    test

--- MainPage_Patient.py
+++ MainPage_Patient.py
@@ -2,10 +2,10 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
-class MainApp(MDApp):
+class HomePagePatient(MDApp):
 	def build(self):
 		self.theme_cls.theme_style = "Dark"
 		self.theme_cls.primary_palette = "BlueGray"
 		return Builder.load_file('MainPage_Patient.kv')
 
-MainApp().run()
+HomePagePatient().run()
