251eff2fb8e3dabf02ceaf5a04a82b019e5b4a3f
ThiaraSana <56861810+ThiaraSana@users.noreply.github.com>
2021-09-10 10:58:00 +0800

    smtest

--- MainPage_Patient.py
+++ MainPage_Patient.py
@@ -2,10 +2,27 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
+
+class WelcomePage(Screen):
+	pass
+
+class DoctorLogin(Screen):
+	pass
+
+class PatientLogin(Screen):
+	pass
+
+ScreenM = ScreenManager()
+
+ScreenM.add_widget(WelcomePage(name='Welcome'))
+ScreenM.add_widget(DoctorLogin(name='DoctorLogin'))
+ScreenM.add_widget(PatientLogin(name='PatientLogin'))
+
+
 class HomePagePatient(MDApp):
 	def build(self):
 		self.theme_cls.theme_style = "Dark"
 		self.theme_cls.primary_palette = "BlueGray"
-		return Builder.load_file('MainPage_Patient.kv')
+		return Builder.load_file('ScreenTest.kv')
 
 HomePagePatient().run()
