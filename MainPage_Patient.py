ca7f4bb0fb66dfd65c54977e6f286ab116dd1971
Jeric <jeric.twy@gmail.com>
2021-09-10 12:02:59 +0800

    10 Sep 2021

--- MainPage_Patient.py
+++ MainPage_Patient.py
@@ -6,17 +6,18 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 class WelcomePage(Screen):
 	pass
 
-class DoctorLogin(Screen):
+class Doctor_LogIn(Screen):
 	pass
 
-class PatientLogin(Screen):
+class Patient_LogIn(Screen):
 	pass
+
 
 ScreenM = ScreenManager()
 
 ScreenM.add_widget(WelcomePage(name='Welcome'))
-ScreenM.add_widget(DoctorLogin(name='DoctorLogin'))
-ScreenM.add_widget(PatientLogin(name='PatientLogin'))
+ScreenM.add_widget(Doctor_LogIn(name='DoctorLogin'))
+ScreenM.add_widget(Patient_LogIn(name='PatientLogin'))
 
 
 class HomePagePatient(MDApp):
