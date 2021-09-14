db84a47096ee73fa7bc6d2a92b8dc0ce3e9e9d4b
ThiaraSana <56861810+ThiaraSana@users.noreply.github.com>
2021-09-14 21:08:30 +0800

    test

--- Sana/Patient_LogIn.py
+++ Sana/Patient_LogIn.py
@@ -2,7 +2,4 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import ScreenManager, SlideTransition
 
 class Patient_LogIn(Screen):
-    def go_back(self):
-        self.parent.transition = SlideTransition(direction="right")
-        self.parent.current = self.parent.current = "welcome_screen"
-        self.parent.transition = SlideTransition(direction="left")
+    pass
