2c2fba875483d765467911d4db4d9014e6a5b20b
Jeric <jeric.twy@gmail.com>
2021-10-15 12:14:58 +0800

    test

---
+++ MainApp/Main.py
@@ -0,0 +1,34 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+import os
+
+class MainApp(MDApp):
+    user_idToken_doctor = ""
+    local_id_doctor = ""
+    user_idToken_patient = ""
+    local_id_patient = ""
+    UserID_Patient = ""
+    UserID_Doctor = ""
+
+
+    Builder.load_file("./Pages/Patient_LogIn.kv")
+    Builder.load_file("./Pages/Doctor_LogIn.kv")
+    Builder.load_file("./Pages/WelcomePage.kv")
+    Builder.load_file("./Pages/PatientHomePage.kv")
+    Builder.load_file("./Pages/DoctorHomePage.kv")
+    Builder.load_file("./Pages/NewUser_Doctor.kv")
+    Builder.load_file("./Pages/NewUser_Patient.kv")
+    Builder.load_file("./Pages/SymptomTracker.kv")
+    Builder.load_file("./Pages/FluidIntake.kv")
+    Builder.load_file("./Pages/PatientList.kv")
+    Builder.load_file("./Pages/BladderDiary.kv")
+
+    def sign_out_doctor(self):
+        self.root.ids.Doctor_LogIn.log_out()
+        self.root.current = 'Doctor_LogIn'
+
+    def sign_out_patient(self):
+        self.root.ids.Patient_LogIn.log_out()
+        self.root.current = 'Patient_LogIn'
+
+MainApp().run()
