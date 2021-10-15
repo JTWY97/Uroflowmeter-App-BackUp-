8e5211de41356673f66d815458dd911d6b4581a6
Jeric <jeric.twy@gmail.com>
2021-10-15 14:33:33 +0800

    did custom fluid entry (needs help to bind to firebase)

--- MainApp/Adapter/FluidIntake.py
+++ MainApp/Adapter/FluidIntake.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
 from kivy.event import EventDispatcher
+from kivymd.uix.textfield import MDTextField
 import numpy as np
 import pyrebase
 
@@ -44,6 +45,10 @@ class FluidIntake(Screen, EventDispatcher):
 			self.showvol()
 			return self.volume
 
+	#def button(self,button):
+		#CustomField = MDTextField(text='Enter Fluid in ml',
+		#pos_hint = {'center_x': 0.5, 'center_y': 0.5}, 
+		#size_hint_x=None,width=300)
 	
 	def showvol(self):
 		print(self.volume)
